9d29476826492cc04c7eaa05cb8a9a982a0861a5
BrootForcer <>
2024-01-23 21:12:34 -0500

    Root Implemented

---
+++ Scenes/Line2D.gd
@@ -0,0 +1,46 @@
+extends Node2D
+
+var distanceFromLastPoint : float = 0
+var speed : float = 100
+var lastPosition : Vector2 = Vector2(0,0)
+var time : float = 0
+var tipPoints : int = 13
+@export var lineRenderer : Line2D
+@export var tip : Line2D
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	tip.add_point(Vector2(0,0))
+	tip.add_point(Vector2(0,0))
+	
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	position += transform.y * speed * delta
+	distanceFromLastPoint += position.distance_to(lastPosition)
+	lastPosition = position
+	var input_dir := Input.get_vector("ui_right", "ui_left", "ui_down", "ui_up")
+	if input_dir:
+		rotate(deg_to_rad(input_dir.x))
+		
+	var size : int = tip.points.size()-1
+	tip.points[size] = position
+	
+	if distanceFromLastPoint >= 25:
+		tip.add_point(position)
+		if size >= tipPoints:
+			
+			tip.remove_point(0)
+		if size >= tipPoints-1:
+			var removingPoint : Vector2 = tip.points[0]
+			lineRenderer.add_point(removingPoint)
+		
+		print(distanceFromLastPoint)
+		distanceFromLastPoint = 0
+		
+
+	pass
