4ff45e5bf7b14585c229132b47269cae941fb744
GarethDa <rath.champo@live.ca>
2024-03-08 18:33:41 -0500

    Switched units to characterbody2d

--- Scripts/Units/Movement Scripts/UnitMove.gd
+++ Scripts/Units/Movement Scripts/UnitMove.gd
@@ -7,13 +7,14 @@ var attacking : bool = false
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if !attacking:
-		get_parent().linear_velocity.x = moveSpeed
+		get_parent().velocity.x = moveSpeed
+	get_parent().move_and_slide()
 	#Debug.Log(get_parent().linear_velocity.x)
 	#print(get_parent().linear_velocity)
 
 func _on_attack_started_attacking():
 	Debug.Log("ENTERED AREA")
-	get_parent().linear_velocity.x = 0
+	get_parent().velocity.x = 0
 	attacking = true
 
 func _on_attack_stopped_attacking():
