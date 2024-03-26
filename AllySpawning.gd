1bb75f5dd8bf682c68046d96da08488e50466973
GarethDa <rath.champo@live.ca>
2024-03-25 23:39:07 -0400

    limited unit spawn

--- AllySpawning.gd
+++ AllySpawning.gd
@@ -1,7 +1,15 @@
 extends PanelContainer
+var numUnits : float
+var unitType : Unit.UnitType
 
 func showAlly(type : Unit.UnitType, amount : int):
+	numUnits = amount
+	unitType = type
 	$VBoxContainer/TextureRect.texture = load("res://Art Assets/Textures/Icons/UndeadUnits/" + str(Unit.UnitType.keys()[type] + ".png"))
 	$VBoxContainer/Label.text = LocalizationManager.tr((Unit.UnitType.keys()[type])) + " : " + str(amount)
 	pass
 	
+
+func useUnit():
+	numUnits -= 1
+	$VBoxContainer/Label.text = LocalizationManager.tr((Unit.UnitType.keys()[unitType])) + " : " + str(numUnits)
--- Scenes/Areas/UnitSpawning.gd
+++ Scenes/Areas/UnitSpawning.gd
@@ -1,18 +1,27 @@
 extends HBoxContainer
 
+var thisRoundUnits : Array[int]
+var allButtons : Array
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var index : int = 0
+	thisRoundUnits = GameManager.availableUnits
 	for unitAmt in GameManager.availableUnits:
 		if unitAmt > 0:
 			var newChild = preload("res://Scenes/UI/AllySpawnButton.tscn").instantiate()
 			newChild.showAlly(index,unitAmt)
 			add_child(newChild)			
 			newChild.find_child("Button").pressed.connect(clicked.bind(index))
+			allButtons.append(newChild)
 		index+=1
 			
 func clicked(typeToSpawn : int):
-	EventManager.onAllySpawned.emit(typeToSpawn)
+	if thisRoundUnits[typeToSpawn] > 0:
+		EventManager.onAllySpawned.emit(typeToSpawn)
+		thisRoundUnits[typeToSpawn] -= 1
+		for buttonObject in allButtons:
+			if buttonObject.unitType == typeToSpawn:
+				buttonObject.useUnit()
 	pass
 
