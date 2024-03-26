f6850ded4444934de9de17a40cc7afe1168a792e
Ryan <ryan.dinh@ontariotechu.net>
2024-03-25 23:44:26 -0400

    Updated Overworld menu

--- Menus/GroundPlacementMenu.gd
+++ Menus/GroundPlacementMenu.gd
@@ -3,48 +3,57 @@ extends PanelContainer
 @export var tower_resources:Array[Resource]
 var most_recent_parentclicked:Node2D
 var open_menu = false
-var display_desc = false
 var towerIndex : int
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$NinePatchRect/MarginContainer/VBoxContainer/Control/Confirm.visible = false
-	$NinePatchRect/MarginContainer/VBoxContainer/TextureRect/ThornwallText.visible = false
-	pass # Replace with function body.
+	$NinePatchRect/MarginContainer/VBoxContainer/Confirm.visible = false
 
 # Cancel button function
 func _on_texture_button_pressed():
-	if !open_menu:
-		return
-	$AnimationPlayer.play("Hide")
-	Debug.Log("I'm hiding")
-	open_menu = false
-	display_desc = false
-	$NinePatchRect/MarginContainer/VBoxContainer/Control/Confirm.visible = false
-	pass # Replace with function body.
+	if open_menu:
+		$AnimationPlayer.play("Hide")
+		open_menu = false
+		$NinePatchRect/MarginContainer/VBoxContainer/Confirm.visible = false
+	
 
+# Node press function
 func _on_button_land_clicked(to_parent_to):
 	if !open_menu:
 		$AnimationPlayer.play("Show")
 		open_menu = true
 	most_recent_parentclicked = to_parent_to
-	pass # Replace with function body.
 
 # Confirm button function
 func _on_button_confirm():
 	most_recent_parentclicked.spawn_tower(tower_resources[towerIndex].instantiate())
 	Debug.Log("spawned tower")
-	$NinePatchRect/MarginContainer/VBoxContainer/Control/Confirm.visible = false
-	$NinePatchRect/MarginContainer/VBoxContainer/TextureRect/ThornwallText.visible = false
-	display_desc = false
-	pass # Replace with function body.
+	$AnimationPlayer.play("Hide")
+	open_menu = false
+	$NinePatchRect/MarginContainer/VBoxContainer/Confirm.visible = false
+	$NinePatchRect/MarginContainer/VBoxContainer/TextureRect/Text0.visible = false
+	$NinePatchRect/MarginContainer/VBoxContainer/TextureRect/Text1.visible = false
+	$NinePatchRect/MarginContainer/VBoxContainer/TextureRect/Text2.visible = false
 
 # General tower button selection function
 func _on_button_clicked(index_arg):
-	if !display_desc:
-		display_desc = true
-		towerIndex = index_arg
+	towerIndex = index_arg
+	
+	match towerIndex:
+		0:
+			$NinePatchRect/MarginContainer/VBoxContainer/TextureRect/Text0.visible = true
+			$NinePatchRect/MarginContainer/VBoxContainer/TextureRect/Text1.visible = false
+			$NinePatchRect/MarginContainer/VBoxContainer/TextureRect/Text2.visible = false
+			Debug.Log("text 1")
+		1:
+			$NinePatchRect/MarginContainer/VBoxContainer/TextureRect/Text0.visible = false
+			$NinePatchRect/MarginContainer/VBoxContainer/TextureRect/Text1.visible = true
+			$NinePatchRect/MarginContainer/VBoxContainer/TextureRect/Text2.visible = false
+			Debug.Log("text 2")
+		2:
+			$NinePatchRect/MarginContainer/VBoxContainer/TextureRect/Text0.visible = false
+			$NinePatchRect/MarginContainer/VBoxContainer/TextureRect/Text1.visible = false
+			$NinePatchRect/MarginContainer/VBoxContainer/TextureRect/Text2.visible = true
+			Debug.Log("text 3")
 		
-		$NinePatchRect/MarginContainer/VBoxContainer/Control/Confirm.visible = true
-		$NinePatchRect/MarginContainer/VBoxContainer/TextureRect/ThornwallText.visible = true
-	pass # Replace with function body.
+	$NinePatchRect/MarginContainer/VBoxContainer/Confirm.visible = true
