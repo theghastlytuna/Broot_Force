fe4f1aa9a398f29f89794f82bf699901c28f5ee3
BrootForcer <>
2024-03-28 16:48:08 -0400

    push with ai?

--- Scenes/Areas/Overworld.gd
+++ Scenes/Areas/Overworld.gd
@@ -1,6 +1,62 @@
 extends Node2D
 
+var inputArray : Array = [
+	[0.4,   0.2, 0.2, 0.2, 0.4, 0.4, 0 ],
+	[0.2,   0,   0,   0,   0,   0,   0 ],
+	[0, 0.2, 0,   0,   0,   0,   0 ],
+	[0, 0,   0.2, 0,   0,   0,   0 ],
+	[0, 0,   0,   0.2, 0,   0,   0 ],
+	[0, 0,   0,   0,   0.2, 0,   0 ],
+	[0, 0,   0,   0,   0,   0.2, 0 ],
+	[0, 0,   0,   0,   0,   0,   0.2 ],
+	[0.2,   0.2, 0,   0,   0,   0,   0 ],
+	[0, 0.2, 0.2, 0,   0,   0,   0 ],
+	[0, 0,   0.2, 0.2, 0,   0,   0 ],
+	[0, 0,   0,   0.2, 0.2, 0,   0 ],
+	[0, 0,   0,   0,   0.2, 0.2, 0 ],
+	[0, 0,   0,   0,   0,   0.2, 0.2 ],
+	[0.2, 0,   0.2, 0,   0,   0,   0 ],
+	[0,   0.2, 0,   0.2, 0,   0,   0 ],
+	[0,   0,  0.2, 0,  0.2, 0,   0 ],
+	[0.2, 0,   0,   0.2, 0,   0,   0 ],
+	[0,   0.2, 0,   0,   0.2, 0,   0 ],
+	[0,   0,   0.2, 0,   0,   0.2, 0 ],
+	[0.2, 0,   0,   0,   0.2, 0,   0 ],
+	[0,   0.2, 0,   0,   0,   0.2, 0 ],
+	[0,   0,   0.2, 0,   0,   0,   0.2 ],
+	[0.2, 0,   0,   0,   0,   0.2, 0 ],
+	[0,   0.2, 0,   0,   0,   0,   0.2 ],
+	[0.2, 0,   0,   0,   0,   0,  0.2 ]
+]
 
+var outputArray : Array = [
+	[0, 0, 0.5, 0, 0, 1, 0.25 ],
+	[0, 0.25, 0, 0, 0.5, 0, 1 ],
+	[0, 0, 0.5, 0.25, 0, 1, 0 ],
+	[0, 0.25, 0, 0, 0.5, 0, 1 ],
+	[0.25, 0, 0, 0.5, 0, 0, 1 ],
+	[0, 0, 0.5, 0, 0, 1, 0.25 ],
+	[0, 0, 0.5, 0.25, 0, 1, 0 ],
+	[0.25, 0, 0, 0.5, 0, 0, 1 ],
+	[0, 0, 0, 0, 0.25, 0.5, 1 ],
+	[0, 0, 0, 0, 0.25, 0.5, 1 ],
+	[0, 0, 0, 0.25, 0.5, 0, 1 ],
+	[0, 0, 0.25, 0, 0, 1, 0.5 ],
+	[0, 0, .5, 0, 0, 1, 0.25 ],
+	[1, 0, 0, 0.25, 0, 0.5, 1 ],
+	[0, 0.25, 0, 0, 0.5, 0, 1 ],
+	[0, 0, 0.25, 1, 0, 0.5, 0 ],
+	[0, 0, 0.5, 0, 0, 1, 0.25 ],
+	[0, 0, 0, 0.5, 0.25, 0, 1 ],
+	[0, 0, 0.5, 0, 0, 1, 0.25 ],
+	[0, 0, 0, 0.25, 0, 0.5, 1 ],
+	[0, 0, 0.25, 0, 0, 0.5, 1 ],
+	[0, 0, 0.5, 0.25, 0, 1, 0 ],
+	[0, 0, 0, 0.25, 0.5, 0, 1 ],
+	[0, 0, 0, 0.25, 0, 0.5, 1 ],
+	[0, 0, 0.25, 0, 0, 1, 0.5 ],
+	[0, 0, 0, 0.5, 0.25, 0, 1 ]
+]
 
 func _enter_tree():
 	GameManager.growthRounds += 1
@@ -8,9 +64,24 @@ func _enter_tree():
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	
+	var newNN : NeuralNetwork = NeuralNetwork.new(7,15,7)
+	newNN.learning_rate = 0.3
+	
+	for epoch in range(200):
+		print(epoch)
+		for i in inputArray.size():
+			
+			newNN.train(inputArray[i],outputArray[i])
+			
+	Debug.Log(newNN.predict(inputArray[0]))	
+	Debug.Log(newNN.predict(inputArray[2]))	
+	Debug.Log(newNN.predict(inputArray[inputArray.size()-1]))
+
+	
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func gameReady():
+	var sendingArray : Array
+	for t in GameManager.placedTowers.keys()
 	pass
