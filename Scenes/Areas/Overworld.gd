cbdc4f2dc099d1b04293089ee7b4855244413c07
BrootForcer <>
2024-04-01 17:42:55 -0400

    final push

--- Scenes/Areas/Overworld.gd
+++ Scenes/Areas/Overworld.gd
@@ -2,11 +2,12 @@ extends Node2D
 
 func _enter_tree():
 	GameManager.growthRounds += 1
+	EventManager.onGrowthPhaseEnd.connect(growthOver)
 	#GameManager.overworldNewRound()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-func gameReady():
-	pass
+func growthOver():
+	get_tree().change_scene_to_file("res://Scenes/UI/landscape_to_portrait.tscn")
